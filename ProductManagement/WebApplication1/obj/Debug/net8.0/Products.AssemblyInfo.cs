17e687a9898335c0fffed986a7982d03e4276ce1
PrathameshDurgude06 <pdurgude63@gmail.com>
2024-07-06 02:44:42 +0530

    WebApp

--- ProductManagement/WebApplication1/obj/Debug/net8.0/Products.AssemblyInfo.cs
+++ ProductManagement/WebApplication1/obj/Debug/net8.0/Products.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa93cffaaa1f57cec71847e09dc538e55e075936")]
 [assembly: System.Reflection.AssemblyProductAttribute("Products")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Products")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
